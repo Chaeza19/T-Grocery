7090ec2751b2201b865d7e826cadb085dbd7643b
Chaeza19 <114897844+Chaeza19@users.noreply.github.com>
2023-12-13 16:03:46 +0700

    Mencoba Welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -841,17 +841,19 @@
                         class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
                 @else
                     <a href="{{ route('login') }}"
-                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log
-                        in</a>
+                        class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Sign In</a>
 
                     @if (Route::has('register'))
                         <a href="{{ route('register') }}"
-                            class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+                            class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Sign Up</a>
                     @endif
                 @endauth
             </div>
         @endif
     </div>
+
+    <div>
+        <img src="{{ asset('assets/svg/Dashboard.svg') }}" alt="Dashboard_SVG">
 </body>
 
 </html>
