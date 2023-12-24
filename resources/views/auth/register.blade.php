02b75eecefaf17f8aad844e3efd2903236fd57e9
marianasamosir <marmar.smsr@gmail.com>
2023-12-25 00:53:31 +0700

    push

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,76 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+<!DOCTYPE html>
+<html>
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Sign Up</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+    <style>
+        .img-signup img{
+            position: absolute;
+            top: 0;
+            right: 0;
+            width: 433px;
+            height: 600px;
+            flex-shrink: 0;
+        }
+        .overlap{
+            max-width: 1200px;
+            display: flex;
+            flex-direction: column; /* Mengatur tata letak menjadi kolom */
+            align-items: center;
+            justify-content: space-between;
+            left: 0;
+            margin-right: 500px;
+        }
+    
+        .overlap-logo img{
+            width: 140px;
+            height: auto;
+            margin-top:60px;
+            margin-bottom: 40px;
+          
+        }
+        .overlap-pembeli img,
+        .overlap-penjual img {
+        width: 480px; /* Sesuaikan dengan lebar yang diinginkan */
+        height: auto; /* Biarkan tinggi mengikuti perubahan lebar untuk mempertahankan rasio aspek */
+        margin-bottom: 10px;
+    }
+    </style>
+
+</head>
+
+<body>
+    <div class = "countainer">
+        <div class = "img-signup">
+            <img src="{{ asset('assets/jpg/signup.jpg') }}" alt="Sign up">
         </div>
 
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        <div class="overlap">
+
+            <div class="overlap-logo">
+                <img src="{{ asset('assets/svg/Logo-tgrocery.svg') }}" alt="Sign up">
+            </div>
+            
+            <div class="group overlap-pembeli">
+                <img src="{{ asset('assets/svg/Costumer.svg') }}" alt="Sign up">
+            </div>
+            
+            <div class="group overlap-penjual">
+                <img src="{{ asset('assets/svg/Owner.svg') }}" alt="Sign up">
+            </div>
+        
+            
         </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+    </div>
+</body>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+</html>
